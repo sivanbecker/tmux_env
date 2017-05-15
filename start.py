9b074fecd45149b388327d5aaca2233233c2212f
sivanbecker <sivanbecker@gmail.com>
2017-05-15 21:27:27 +0300

    different tools dont act the same when entering tmux env

--- start.py
+++ start.py
@@ -1,4 +1,4 @@
-#!/Users/sbecker/envs/.default_env_3/bin/python3
+#!/usr/local/bin/python3
 
 import os
 
@@ -10,50 +10,98 @@ _logger = logbook.Logger(__name__)
 
 ################# GENERAL FUNCS #############
 
+def _cob_dev_export():
+    return 'unset __PYVENV_LAUNCHER__ && export COB_DEVELOP=1 && export COB_NO_REENTRY=1'
+
+def _get_flask_env_command(dir):
+    return 'cd {} && source ../.env/bin/activate && unset __PYVENV_LAUNCHER__ && export FLASK_DEBUG=1 && export FLASK_APP=app1.py'.format(dir)
+
+def _get_cob_env_command(dir):
+    return 'cd {} && source ../.env/bin/activate && {}'.format(dir, _cob_dev_export())
+
+def _get_infradev_env_command(dir):
+    update_itzik = 'itzik update -env {}'.format(dir+'/env')
+    return 'cd {} && source env/bin/activate && {}'.format(dir, update_itzik)
+
+
 def _get_env_command(dir):
-    return 'cd {} && source ../.env/bin/activate'.format(dir)
+    if os.path.exists(os.path.realpath(os.curdir)+'/../.env'):
+        return 'cd {} && source ../.env/bin/activate'.format(dir)
+    elif os.path.exists(os.path.realpath(os.curdir)+'/env'):
+        return 'cd {} && source env/bin/activate'.format(dir)
 
 def _get_pane_command(cmds):
 
     return '&&'.join(list(filter(None, cmds)))
 
-def  _windows(names=[], pane_cmds=[], work_dir=None, layout='even-horizontal' ):
+def  _windows(names=[], pane_cmds=[], work_dir=None, layout='even-horizontal', tool=None):
     w = dict()
     w['windows'] = []
     if not len(names) == len(pane_cmds):
         raise ValueError('Windows and commands are not the same length {}:{}'.format(names,pane_cmds))
     for index,name in enumerate(names):
-        w['windows'].append({'window_name': name, 'layout':layout, 'panes':[_get_pane_command([_get_env_command(work_dir),pane_cmds[index]]),]})
+        if tool:
+            if tool=='dhcpawn':
+                w['windows'].append({'window_name': name, 'layout':layout, 'panes':[_get_pane_command([_get_cob_env_command(work_dir),pane_cmds[index]]),]})
+            elif tool=='infradev':
+                w['windows'].append({'window_name': name, 'layout':layout, 'panes':[_get_pane_command([_get_infradev_env_command(work_dir),pane_cmds[index]])]})
+            elif tool=='flask':
+                w['windows'].append({'window_name': name, 'layout':layout, 'panes':[_get_pane_command([_get_flask_env_command(work_dir),pane_cmds[index]])]})
+            else:
+                w['windows'].append({'window_name': name, 'layout':layout, 'panes':[_get_pane_command([_get_env_command(work_dir),pane_cmds[index]])]})
+
     w['path'] = os.path.dirname(work_dir)
     w['env'] = w['path']+'.env'
 
     return w
 
 
+
 ################# TEMPLATES ###############
 tdict = {}
 tdict['infinilab'] = _windows(
-    names=['shell', 'runserver', 'infinilab', 'dmns', 'db', 'terminal'],
-    pane_cmds=['./manage.py shell','./manage.py runserver 5005','ssh infinilab', 'ssh infinilab-daemons','ssh infinilab-db',''],
-    work_dir='/Users/sbecker/envs/infinilab/infinilab')
+    names=['shell', 'runserver', 'infinilab', 'dmns', 'db', 'trm1', 'trm2', 'trm3'],
+    pane_cmds=['./manage.py shell','./manage.py runserver 5005','ssh infinilab', 'ssh infinilab-daemons','ssh infinilab-db','', '', ''],
+    work_dir='/Users/sbecker/envs/infinilab/infinilab',
+    tool='infinilab')
 
 tdict['bro'] = _windows (
     names=['bro', 'bro-db', 'local-bro'],
     pane_cmds=['ssh bro', 'ssh bro-db', ''],
-    work_dir='/Users/sbecker/envs/bro/bro'
+    work_dir='/Users/sbecker/envs/bro/bro',
+    tool='bro'
     )
 
 tdict['dhcpawn'] = _windows(
-    names=['testserver', 'pgcli', 'terminal', 'celery worker', 'rabbitmq-server'],
-    pane_cmds=['cob testserver','pgcli dhcpawn','', 'celery -A cob.celery_utils worker --loglevel=DEBUG -E -B','rabbitmq-server'],
-    work_dir='/Users/sbecker/envs/dhcpawn/dhcpawn/')
+    names=['testserver', 'pgcli', 'celery worker', 'rabbitmq-server', 'trm1', 'trm2', 'trm3'],
+    pane_cmds=['cob testserver','pgcli dhcpawn', 'celery -A cob.celery_utils worker --loglevel=DEBUG -E -B','rabbitmq-server', '', '', ''],
+    work_dir='/Users/sbecker/envs/dhcpawn/dhcpawn/',
+    tool='dhcpawn')
 
 
-tdict['p2'] = _windows(names=['term3'], pane_cmds=['ls -l'], work_dir='/Users/sbecker/envs/dhcpawn/dhcpawn/')
+tdict['p2'] = _windows(names=['term3'], pane_cmds=['ls -l'], work_dir='/Users/sbecker/envs/p2/p2', tool='p2')
 
-p3 = []
+tdict['p3'] = _windows(
+    names=['trm1', 'trm2', 'trm3'],
+    pane_cmds=['','',''],
+    work_dir='/Users/sbecker/envs/p3/p3',
+    tool='p3')
 
-infradev = []
+
+tdict['infradev'] = _windows(names=['infradev_cli_1','infradev_cli_2','trm3'],
+                             pane_cmds=['cd infradev-cli/infradev_cli', 'cd infradev-cli/infradev_cli', ''],
+                             work_dir='/Users/sbecker/envs/itzik/infradev/',
+                             tool='infradev')
+
+tdict['flask'] = _windows(names=['trm1','trm2','trm3'],
+                          pane_cmds=['','',''],
+                          work_dir='/Users/sbecker/envs/flask/app1',
+                          tool='flask')
+
+tdict['meeseeks'] = _windows(names=['trm1','trm2','trm3'],
+                          pane_cmds=['','',''],
+                          work_dir='/Users/sbecker/envs/flask/meeseeks',
+                          tool='meeseeks')
 
 itzik = []
 
@@ -69,6 +117,7 @@ def develop(tool):
     with open(tmux_config_filename, 'w') as f:
         yaml.dump(_get_tmux_config(tool), f)
     os.chdir(tdict[tool]['path'])
+    # import pudb;pudb.set_trace()
     os.execve(tmuxp, [tmuxp, 'load', tmux_config_filename], {**os.environ})
 
 
